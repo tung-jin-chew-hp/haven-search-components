01c69ab31177cf0b61afda2cde1c9d5585604155
Tung Jin Chew <tung-jin.chew@hpe.com>
2017-02-03 12:31:49 +0000

    Add a check to expose the editable fields; so we can prevent users from modifying arbitrary fields

--- core/src/main/java/com/hp/autonomy/searchcomponents/core/search/fields/AbstractDocumentFieldsService.java
+++ core/src/main/java/com/hp/autonomy/searchcomponents/core/search/fields/AbstractDocumentFieldsService.java
@@ -13,6 +13,8 @@ import com.hp.autonomy.searchcomponents.core.config.HavenSearchCapable;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
 
 /**
  * Skeletal implementation of {@link DocumentFieldsService} containing a default implementation of {@link DocumentFieldsService#getPrintFields(Collection)}
@@ -41,5 +43,20 @@ public abstract class AbstractDocumentFieldsService implements DocumentFieldsSer
                 .forEach(field -> fields.addAll(field.getNames()));
 
         return fields;
+    }
+
+    @Override
+    public Set<String> getEditableIdolFields(final String field) {
+        final FieldsInfo fieldsInfo = configService.getConfig().getFieldsInfo();
+
+        final FieldInfo<?> fieldInfo = fieldsInfo.getFieldConfig().get(field);
+
+        final Set<String> fieldNames = new TreeSet<>(String.CASE_INSENSITIVE_ORDER);
+
+        if (fieldInfo != null && !fieldInfo.getEditable().isEmpty()) {
+            fieldNames.addAll(fieldInfo.getNames());
+        }
+
+        return fieldNames;
     }
 }
